82b481bc0eca772c7c7913a98bf0e74625a31cf5
Lab05_etec <Lab05_etec@etec>
2025-02-11 20:21:33 -0300

    criacao arquivo contato.php

--- contato.php
+++ contato.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá Mundo</title>
+    <title>Entre em contato</title>
 </head>
 <body>
     <?php
-        echo "Hello world";
+        echo "<h1>Página de contato</h1>";
     ?>
 </body>
 </html>
