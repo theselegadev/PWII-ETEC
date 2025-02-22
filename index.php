1c7d847a2561e4f879089b9c51bf9f5486559c67
Lab05_etec <Lab05_etec@etec>
2025-02-21 22:28:55 -0300

    aula 21/02

--- index.php
+++ index.php
@@ -7,23 +7,42 @@
     <title>Olá Mundo</title>
     <link href="bootstrap.min.css" rel="stylesheet">
 </head>
-<style>
-  .row > div{
-    padding: 50px;
-  }
-</style>
 <body>
-    <div class="row">
-      <div class="col-4">
-        <button type="button" class="btn btn-primary">Cadastrar</button>
-      </div>
-      <div class="col-4">
-          <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-      </div>
-      <div class="col-4">
-        <button class="btn btn-outline-success" type="submit">Procurar</button>
+  <div class="row">
+    <div class="col">
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="/PWII-ETEC/imagens/bootstrap-logo.svg" alt="icone" heigth="24" width="30">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Php
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Variável</a></li>
+              <li><a class="dropdown-item" href="#">If</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">While</a></li>
+              <li><a class="dropdown-item" href="#">Switch</a></li>
+              <li><a class="dropdown-item" href="#">Array/Vetor</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </div>
+  </nav>
+    </div>
+  </div>
     <script src="bootstrap.bundle.min.js" ></script>
 </body>
 </html>
