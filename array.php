750c67820a514296a58b27340b18158de401f7f8
Lab05_etec <Lab05_etec@etec>
2025-03-11 20:36:34 -0300

    Array

--- array.php
+++ array.php
@@ -11,6 +11,60 @@
     <link href="bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="overflow-x:hidden">
+    <div class="container">
+        <h1>Array</h1>
+        <p>São variavéis que podem guardar uma lista de valores identificados por indice ou chave.</p>
+        <pre>
+            $frutas = ["Banana","Maça","Abacaxi","Mamão"];
+            //ou
+            $frutas = array("Banana","Maça","Abacaxi","Mamão");
+        </pre>
+        <?php
+            $frutas = array("Banana","Maça","Abacaxi","Mamão");
+            echo "<br>$frutas[2]";
+
+            for($i = 0; $i < count($frutas); $i++){
+                echo "<br>$frutas[$i]";
+            }
+
+            array_push($frutas,"Kiwi");
+            array_push($frutas,rand(1,7));
+
+            for($i = 0; $i < count($frutas); $i++){
+                echo "<br>$frutas[$i]";
+            }
+
+            $b = array(rand(1,15),rand(1,15),rand(1,15),rand(1,15),rand(1,15));
+            $i = array(rand(16,30),rand(16,30),rand(16,30),rand(16,30),rand(16,30));
+            $n = array(rand(31,45),rand(31,45),rand(31,45),rand(31,45),rand(31,45));
+            $g = array(rand(46,60),rand(46,60),rand(46,60),rand(46,60),rand(46,60));
+            $o = array(rand(61,75),rand(61,75),rand(61,75),rand(61,75),rand(61,75))
+        ?>
+
+        <table style="border: 2px solid black;">
+            <thead>
+                <th>B</th>
+                <th>I</th>
+                <th>N</th>
+                <th>G</th>
+                <th>O</th>
+            </thead>
+            <tbody>
+                <?php
+                    for($in=0;$in<5;$in++){
+                        echo "<tr>";
+                        echo "<td>$b[$in]</td>";
+                        echo "<td>$i[$in]</td>";
+                        echo "<td>$n[$in]</td>";
+                        echo "<td>$g[$in]</td>";
+                        echo "<td>$o[$in]</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
     <script src="bootstrap.bundle.min.js" ></script>
 </body>
 </html>
