c767c10a44574669d8dd6191d0913a2e78b16356
Lab05_etec <Lab05_etec@etec>
2025-05-06 20:23:49 -0300

    Ajuste

--- nav.php
+++ nav.php
@@ -24,6 +24,7 @@
               <li><a class="dropdown-item" href="for.php">For</a></li>
               <li><a class="dropdown-item" href="array.php">Array/Vetor</a></li>
               <li><a class="dropdown-item" href="formulario.php">Formulário</a></li>
+              <li><a class="dropdown-item" href="index.php">Home</a></li>
               <li><a class="dropdown-item" href="login.php">Login</a></li>
               <li><a class="dropdown-item" href="usuarios.php">Usuarios</a></li>
             </ul>
