fe796212a13f5b851549e7a49b686d611c3919e8
theselegadev <matheus.seleghin7@gmail.com>
2025-05-30 17:17:16 -0300

    Ajustes

--- CRUD/App/Model/Conexao.php
+++ CRUD/App/Model/Conexao.php
@@ -8,6 +8,7 @@
             if(!isset(self::$conexao)){
                 try{
                     self::$conexao = new \PDO("mysql:host=localhost;dbname=test;charset=utf8mb4","root","");
+                    self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 }catch(\PDOException $e){
                     echo "Erro de conexão: " . $e->getMessage();
                 }
